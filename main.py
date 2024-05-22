6ec744f33b456a6dd167630ebbc1000c2b8e5b76
X-Caboclean <146889041+xcaboclean@users.noreply.github.com>
2024-05-22 00:54:40 +0000

    type

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from prettytable import PrettyTable
 table = PrettyTable()
 
 table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-
+table.add_column("Pokemon Type", ["Electric", "Water", "Fire"])
 print(table)
